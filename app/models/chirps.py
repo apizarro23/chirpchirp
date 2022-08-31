8ff347e42cf6b11b028deb0835bafc74e7f9e984
Antony Pizarro <apizarro23@live.com>
2022-08-31 11:17:10 -0400

    began dev on chirps

---
+++ app/models/chirps.py
@@ -0,0 +1,12 @@
+from time import timezone
+from .db import db
+
+
+class Chirp(db.Model):
+    __tablename__ = 'chirps'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    chirps = db.Column(db.Varchar(280), nullable=False)
+    image_url = db.Column(db.Varchar, nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), db.func.now())
