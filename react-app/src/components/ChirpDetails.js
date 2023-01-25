ea584a2ff64313c96cce9fc342c08ee8efd7be98
Antony Pizarro <apizarro23@live.com>
2023-01-25 12:26:53 -0500

    added like to chirp details

--- react-app/src/components/ChirpDetails.js
+++ react-app/src/components/ChirpDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { getChirps } from "../store/chirps";
+import { getChirps, likeChirp } from "../store/chirps";
 import { getComments } from "../store/comments";
 import EditChirpModal from "./ChirpFeed/EditChirpModal";
 import DeleteChirpModal from "./ChirpFeed/DeleteChirpModal";
@@ -13,6 +13,9 @@ import linked from "./images/linked-in-logo.png"
 import git from "./images/github-logo.png"
 import defaultImage from "./images/icons8-bird-96.png"
 
+import solidHeart from "./images/solid_heart.svg"
+import hollowHeart from "./images/hollow_heart.svg"
+
 const ChirpDetails = () => {
     const [users, setUsers] = useState([]);
     const [editActive, setEditActive] = useState(false)
@@ -22,7 +25,15 @@ const ChirpDetails = () => {
     const dispatch = useDispatch()
     const user = useSelector(state => state?.session.user)
     const chirp = useSelector(state => state?.chirps[chirpId])
+    const chirps = useSelector((state) => Object.values(state?.chirps));
     // const comments = useSelector(state => Object.values(state?.comments))
+
+    const [isLikedByUser, setIsLikedByUser] = useState(false);
+    const [likeCounter, setLikeCounter] = useState(chirp?.likes?.length);
+    const [likesArray, setLikesArray] = useState(chirp?.likes ?? []);
+    const [likedChirp, setLikedChirp] = useState(
+      likesArray?.find((like) => like.user_id === user.id)
+    );
 
     // console.log("LOOK HERE!!!!!!!!!!!!", )
 
@@ -42,6 +53,31 @@ const ChirpDetails = () => {
           fetchData();
         
     }, [dispatch, chirpId])
+
+    useEffect(() => {
+        if (likedChirp) {
+          setIsLikedByUser(true);
+        }
+      }, [likesArray.length, likedChirp]);
+    
+      if (!chirps) return null;
+    
+      const handleLike = async (e) => {
+        e.stopPropagation();
+        if (isLikedByUser) {
+          const unliked = fetch(`/api/likes/${likedChirp.id}`, {
+            method: "DELETE",
+          });
+          setIsLikedByUser(false);
+          setLikeCounter((prev) => prev - 1);
+          setLikedChirp(null);
+        } else {
+          const like = await dispatch(likeChirp(chirp.id));
+          setIsLikedByUser(true);
+          setLikedChirp(like);
+          setLikeCounter((prev) => prev + 1);
+        }
+      };
 
     // console.log("THESE ARE THE USERS", users)
 
@@ -86,13 +122,28 @@ const ChirpDetails = () => {
                 </div>
                 <div className="chirp-content">
                     <div className="chirp-wrap">
-                {chirp?.chirp_content}
+                        {chirp?.chirp_content}
+
+                    <div onClick={handleLike} className="heart-icon-container">
+                        <img
+                        className={`buzz icon heart ${
+                            likedChirp ? "liked" : "not-liked"
+                        }`}
+                        src={likedChirp ? solidHeart : hollowHeart}
+                        alt="heart-icon"
+                    />
+                  </div>
+                  {/* <div className="like-counter">
+                    <span>{likeCounter}</span>
+                  </div> */}
+                        
                     </div>
-                <img src={chirp?.image_url} className="single-chirp-img" alt="" onError={ e => {
+                            <img src={chirp?.image_url} className="single-chirp-img" alt="" onError={ e => {
                             if (chirp.image_url) {
                                 e.currentTarget.src = defaultImage
                             }
-                        }} />
+                            }} />
+                            
                 </div>
                 </div>
                 ):(
@@ -111,12 +162,35 @@ const ChirpDetails = () => {
                         </Link>
                     </div>
                     <div className="chirp-content">
-                      <div>{chirp?.chirp_content}</div>
+                        <div className="chirp-wrap">
+                            {chirp?.chirp_content}
+
+                            <div onClick={handleLike} className="heart-icon-container">
+                        <img
+                        className={`buzz icon heart ${
+                            likedChirp ? "liked" : "not-liked"
+                        }`}
+                        src={likedChirp ? solidHeart : hollowHeart}
+                        alt="heart-icon"
+                    />
+                                      </div>
+
+
+                        </div>
+                            
+                            
+                        </div>
+
+                      <div>
+                        
                       <img src={chirp?.image_url} className="single-chirp-img" alt="" onError={ e => {
                             if (chirp.image_url) {
                                 e.currentTarget.src = defaultImage
                             }
-                        }} />                    </div>
+                        }} /> 
+
+
+                        </div>
                   </div>  
                 )}
                 <div className="comments-container">
@@ -132,5 +206,6 @@ const ChirpDetails = () => {
         </div>
     )
 }
+
 
 export default ChirpDetails
